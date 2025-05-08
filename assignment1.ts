2be7fc902621eaa5ad0fd1d90813a19b32906ef0
sadiabinta <sadiaa.kabir@gmail.com>
2025-05-07 21:53:12 -0400

    3,4 done

--- assignment1.ts
+++ assignment1.ts
@@ -25,5 +25,47 @@ const books = [
   ];
   
   console.log(filterByRating(books)); 
-  // Output: [ { title: "Book A", rating: 4.5 }, { title: "Book C", rating: 5.0 } ]
 
+  // ##########################//
+
+  function concatenateArrays<T>(...arrays: T[][]): T[]{
+    let concatValue:T[]=[];
+    for (const array of arrays){
+        concatValue=[...concatValue,...array]
+    }
+    return concatValue;
+        
+  }
+
+console.log(concatenateArrays(["a", "b"], ["c"]));
+console.log(concatenateArrays([1, 2], [3, 4], [5]));
+
+// ##########################//
+
+class Vehicle{
+    private _make:string;
+    private _year:number;
+
+    constructor(_make:string,_year:number){
+        this._make=_make;
+        this._year=_year;
+    }
+
+    getInfo(){
+        console.log(`Make: ${this._make}, Year: ${this._year}`);
+    }
+
+}
+class Car extends Vehicle{
+    private _model:string;
+    constructor(_make:string,_year:number,_model:string){
+        super(_make,_year);
+        this._model=_model;
+    }
+    getModel(){
+        console.log(`Model: ${this._model}`);
+    }
+}
+const myCar = new Car("Toyota", 2020, "Corolla");
+myCar.getInfo();   // Output: "Make: Toyota, Year: 2020"
+myCar.getModel();  // Output: "Model: Corolla"
